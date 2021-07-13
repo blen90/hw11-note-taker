271a7faf57b076f0380ba457cc7198262cfc4592
Blenda Orellana <blen.or90@gmail.com>
2021-07-13 10:11:50 -0500

    getting the right information from the page

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 // Dependencies
-const path = require("path");
 const express = require('express');
 const fs = require('fs');
-const exampleNote = require('./db/db.json');
+const dbNote = require('./db/db.json');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const { stringify } = require("querystring");
-uuidv4();
 
 
 const app = express();
@@ -17,41 +15,54 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//Routes
 
 // Routes
 
 //Route to index.html file
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-//Route to notes.html file
+
+
+ //Route to notes.html file
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 //Route to db file
 
 app.get('api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
 
-//Route * to index.html
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 //Get note from db file
 
 app.post('/api/notes', (req, res) => {
-    const exampleNote = JSON.parse(fs.readFileSync ("./db/db.json"));
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    exampleNote.push(newNote);
-    console.log(newNote);
-    console.log(newNote.id);
-    fs.writeFileSync("./db/db.json", JSON.stringify(exampleNote))
-    res.json(exampleNote);
+    const dbNote = JSON.parse(fs.readFileSync ("./db/db.json"));
+    const newEntry = req.body;
+    req.body.id = uuidv4();
+    console.log(newEntry);
+    // console.log(newEntry.body);
+    // console.log(newEntry.id);
+
+
+    dbNote.push(newEntry);
+
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbNote));
+    res.json(dbNote);
 });
 
-// app.delete('/api/notes', (req, res) => {
-//     const existingNote = req.params.id;
-//     fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote))
-//     res.json(existingNote);
-// });
+app.delete('/api/notes/:id', (req,res) => {
+   const { id } = req.params.id;
+
+   const deletedNote = dbNote.find(dbNote => dbNote.id === id)
+   if(deletedNote) {
+        dbNote = dbNote.filter(dbNote => dbNote.id =! id)
+   }else {
+
+   }
+   res.status(404).json({message: "Deleted note doesn't exist"});
+
+   fs.writeFileSync("./db/db.json", JSON.stringify(deletedNote));
+    res.json(deletedNote);
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
