97af212ffca9bbdb7446c6fb501be4b6398076fe
Blenda Orellana <blen.or90@gmail.com>
2021-07-13 16:06:54 -0500

    changing things around to see how to render notes

--- server.js
+++ server.js
@@ -1,68 +1,80 @@
 // Dependencies
 const express = require('express');
 const fs = require('fs');
-const dbNote = require('./db/db.json');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const existingNote = require('./db/db.json');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-
-// Routes
+// ROUTES GET METHOD
 
 //Route to index.html file
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
-
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
  //Route to notes.html file
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/notes', (req, res) => { 
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 //Route to db file
+app.get('api/notes', (req, res) => { 
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
 
-app.get('api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+//Route to Html file
+app.get('*', (req, res) => { 
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
-
-//Get note from db file
-
+//POST MEHTHOD
 app.post('/api/notes', (req, res) => {
-    const dbNote = JSON.parse(fs.readFileSync ("./db/db.json"));
+
+    const existingNote = JSON.parse(fs.readFileSync ("./db/db.json"));
     const newEntry = req.body;
-    req.body.id = uuidv4();
+    newEntry.id = uuidv4();
+    
     console.log(newEntry);
-    // console.log(newEntry.body);
-    // console.log(newEntry.id);
+    console.log(newEntry.id);
 
+  
+    // PUSH newEntry to existingNote
+    existingNote.push(newEntry);
 
-    dbNote.push(newEntry);
-
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(dbNote));
-    res.json(dbNote);
+    
+    fs.writeFileSync("./db/db.json", JSON.stringify(existingNote));
+    res.json(existingNote);
 });
 
 app.delete('/api/notes/:id', (req,res) => {
-   const { id } = req.params.id;
+    const { id } = req.params.id;
+ 
+    const delNote = existingNote.find(existingNote => existingNote.id === id)
+    if(delNote) {
+         existingNote = existingNote.filter(existingNote => existingNote.id =! id)
+    }else {
+ 
+    }
+    res.status(404).json({message: "Note doesn't exist"});
+ 
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+     res.json(delNote);
+ });
+ 
 
-   const deletedNote = dbNote.find(dbNote => dbNote.id === id)
-   if(deletedNote) {
-        dbNote = dbNote.filter(dbNote => dbNote.id =! id)
-   }else {
 
-   }
-   res.status(404).json({message: "Deleted note doesn't exist"});
 
-   fs.writeFileSync("./db/db.json", JSON.stringify(deletedNote));
-    res.json(deletedNote);
-})
+
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
